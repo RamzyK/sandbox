a85b1617e06c79e52c0ce8a92fd12bf38bfadc9d
Kermad <gkermad.ramzy@gmail.com>
2018-04-09 15:37:53 +0200

    initial

---
+++ src/test/my-debug-test.ts
@@ -0,0 +1,1 @@
+export const testToDebug = 'should say hello';
---
+++ src/test/test.spec.ts
@@ -0,0 +1,27 @@
+// tslint:disable:only-arrow-functions
+// tslint:disable-next-line:no-implicit-dependencies
+import * as chai from 'chai';
+import './debug-test';
+// tslint:disable-next-line:no-implicit-dependencies
+import 'mocha';
+import { HelloWorld } from '../lib/hello-world';
+
+const expect = chai.expect;
+// const assert = chai.assert;
+describe('Hello World', function() {
+
+    it('should say hello', function() {
+        const helloWorld = new HelloWorld();
+        const name = 'John';
+        const actual = helloWorld.sayHello(name);
+        const expected = 'Hello John';
+        expect(actual).equals(expected);
+    });
+    it('x', function() {
+        const helloWorld = new HelloWorld();
+        const name = 'John';
+        const actual = helloWorld.sayHello(name);
+        const expected = 'Hello John';
+        expect(actual).equals(expected);
+    });
+});
